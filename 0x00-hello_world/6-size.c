8143265ff13dab19001c22fdb6806488e2b1ed63
Kayode Oluwajuwon Olaoluwa <kayodeoluwajuwon9@gmail.com>
2022-12-12 03:00:52 -0800

    sdwsfss

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+
 	return (0);
 }
